aa4b5e51301de4e84009e68e6ebd71daddcecbad
KW <kee.wei26@gmail.com>
2023-08-10 18:00:59 +0100

    added more tests

--- test/game.test.js
+++ test/game.test.js
@@ -1,10 +1,11 @@
 import { expect } from "chai";
 import { beforeEach, describe, it } from "mocha";
-import Game from "../src/game.js";
-import Player from "../src/player.js";
 import chai from "chai";
 import chaiHttp from "chai-http";
+
 import app from "../app.js";
+import Game from "../src/game.js";
+import Player from "../src/player.js";
 
 chai.use(chaiHttp);
 const TESTPATH = "/game";
@@ -12,21 +13,43 @@ const TESTPATH = "/game";
 describe("Game tests", () => {
   let res;
 
-  beforeEach(() => {
+  afterEach(() => {
     res = null;
   });
 
-  describe("Single player case", () => {
-    it("should render single.ejs when GET /game was hit with query single player", async () => {
-      res = await chai.request(app).get(TESTPATH).query({ player: "single" });
+  it("should render single.ejs when GET /game was hit with query single player", async () => {
+    res = await chai.request(app).get(TESTPATH).query({ player: "single" });
 
-      expect(res).to.have.status(200);
-      expect(res).to.be.html;
-      expect(res.text).to.have.string("Player 1:");
-      expect(res.text).to.have.string('value="single"');
+    expect(res).to.have.status(200);
+    expect(res).to.be.html;
+    expect(res.text).to.have.string("Player 1:");
+    expect(res.text).to.have.string('value="single"');
+  });
+
+  it("should render multi.ejs when GET /game was hit with query multi player", async () => {
+    res = await chai.request(app).get(TESTPATH).query({ player: "multi" });
+
+    expect(res).to.have.status(200);
+    expect(res).to.be.html;
+    expect(res.text).to.have.string("Player 1:");
+    expect(res.text).to.have.string("Player 2:");
+    expect(res.text).to.have.string('value="multi"');
+  });
+
+  it("should render the end page when /end gets hit", async () => {
+    res = await chai.request(app).post(`${TESTPATH}/start`).type("form").send({
+      gameType: "single",
+      player1: "squidward",
     });
 
-    it("should setup a Game instance with player 1 and BOT in app.locals when POST /start is hit with form submission of player 1's name", async () => {
+    res = await chai.request(app).get(`${TESTPATH}/end`);
+
+    expect(res).to.have.status(200);
+    expect(res.text).to.have.string("Rematch");
+  });
+
+  describe("Single player case", () => {
+    beforeEach(async () => {
       res = await chai
         .request(app)
         .post(`${TESTPATH}/start`)
@@ -35,7 +58,9 @@ describe("Game tests", () => {
           gameType: "single",
           player1: "squidward",
         });
+    });
 
+    it("should setup a Game instance with player 1 and BOT in app.locals when POST /start is hit with form submission of player 1's name", async () => {
       expect(res).to.have.status(200);
       expect(res).to.be.html;
 
@@ -47,11 +72,8 @@ describe("Game tests", () => {
       expect(app.locals.game.round).to.equal(1);
     });
 
-    it("should increment round number when a game is played ", async () => {
-      await chai.request(app).post(`${TESTPATH}/start`).type("form").send({
-        gameType: "single",
-        player1: "squidward",
-      });
+    it("should render roundResult page when /play-round gets hit and show BOT has made a choice", async () => {
+      expect(app.locals.game.players[1].choice).to.be.null;
 
       res = await chai
         .request(app)
@@ -62,22 +84,32 @@ describe("Game tests", () => {
         });
 
       expect(res).to.have.status(200);
+      expect(app.locals.game.players[0].choice).to.equal("rock");
+      expect(app.locals.game.players[1].choice).to.not.be.null;
+      expect(res.text).to.not.have.string("<input");
+      expect(res.text).to.have.string("/next-round");
+    });
+
+    it("should increment round 1 to round 2 when /next-round gets hit", async () => {
+      res = await chai
+        .request(app)
+        .post(`${TESTPATH}/play-round`)
+        .type("form")
+        .send({
+          p1Choice: "rock",
+        });
+
+      expect(app.locals.game.round).to.equal(1);
+
+      res = await chai.request(app).get(`${TESTPATH}/next-round`);
+
+      expect(res).to.have.status(200);
       expect(app.locals.game.round).to.equal(2);
     });
   });
 
   describe("Two players case", () => {
-    it("should render multi.ejs when GET /game was hit with query multi player", async () => {
-      res = await chai.request(app).get(TESTPATH).query({ player: "multi" });
-
-      expect(res).to.have.status(200);
-      expect(res).to.be.html;
-      expect(res.text).to.have.string("Player 1:");
-      expect(res.text).to.have.string("Player 2:");
-      expect(res.text).to.have.string('value="multi"');
-    });
-
-    it("should setup a Game instance with both player 1 and 2 in app.locals when POST /start is hit with form submission of both player's name", async () => {
+    beforeEach(async () => {
       res = await chai
         .request(app)
         .post(`${TESTPATH}/start`)
@@ -87,7 +119,9 @@ describe("Game tests", () => {
           player1: "squid",
           player2: "ward",
         });
+    });
 
+    it("should setup a Game instance with both player 1 and 2 in app.locals when POST /start is hit with form submission of both player's name", async () => {
       expect(res).to.have.status(200);
       expect(res).to.be.html;
 
@@ -98,7 +132,37 @@ describe("Game tests", () => {
       expect(app.locals.game.gameType).to.equal("multi");
       expect(app.locals.game.round).to.equal(1);
     });
-  });
 
-  it("should end the game and render the end page", () => {});
+    it("should render end page with the winner's (player 1) name when /end gets hit", async () => {
+      res = await chai
+        .request(app)
+        .post(`${TESTPATH}/play-round`)
+        .type("form")
+        .send({
+          p1Choice: "rock",
+          p2Choice: "scissors",
+        });
+
+      res = await chai.request(app).get(`${TESTPATH}/end`);
+
+      expect(res).to.have.status(200);
+      expect(res.text).to.have.string("squid!");
+    });
+
+    it("should render end page with the winner's (player 1) name when /end gets hit", async () => {
+      res = await chai
+        .request(app)
+        .post(`${TESTPATH}/play-round`)
+        .type("form")
+        .send({
+          p1Choice: "lizard",
+          p2Choice: "rock",
+        });
+
+      res = await chai.request(app).get(`${TESTPATH}/end`);
+
+      expect(res).to.have.status(200);
+      expect(res.text).to.have.string("ward!");
+    });
+  });
 });
